dc466dafe93a4ed3de1e3ed01bef7dcf26bd0ad4
Tanveer Abdul <abdul.tanveer.j@gmail.com>
2026-05-11 17:40:49 +0530

    Cleanup: Remove placeholder PDF and update print styles

--- components/ResultsDashboard.tsx
+++ components/ResultsDashboard.tsx
@@ -228,7 +228,7 @@ export default function ResultsDashboard({ result, fileName, onReset }: Props) {
       </div>
 
       {/* Interactive Action Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-16 action-cards-container">
         {actionCards.map((a, i) => (
           <div
             key={a.id}
